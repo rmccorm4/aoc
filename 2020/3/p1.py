79d3b791f108da497870a9a91374e53f46b9cd2b
Ryan McCormick <mccormick.codes@gmail.com>
2020-12-03 01:45:59 -0800

    day3 cleanup

--- 2020/3/p1.py
+++ 2020/3/p1.py
@@ -1,41 +1,27 @@
-#!/usr/bin/env python3
-import math
-import numpy as np
-from collections import namedtuple
-
-def preprocess_input(filename, slope_x, slope_y):
+def solution(filename, slopes):
     with open(filename) as f:
-        arr = np.array([list(line.strip()) for line in f])
+        arr = [list(row.strip()) for row in f]
+        height, width = len(arr), len(arr[0])
+        print("Input (Height, Width): {}".format((height, width)))
 
-    height, width = arr.shape[0], arr.shape[1]
-    num_moves = math.ceil(height / slope_y)
-    necessary_width = num_moves * slope_x
-    num_copies = math.ceil(necessary_width / width)
+    solutions = []
+    for slope in slopes:
+        num_trees = 0
+        x, y = 0, 0
+        slope_x, slope_y = slope[0], slope[1]
+        while y < height:
+            if arr[y][x] == "#":
+                num_trees += 1
 
-    print("Slope: ({}, {})".format(slope_x, slope_y))
-    print("\tOriginal Input Shape: {}".format(arr.shape))
-    print("\tNum horizontal copies needed: {}".format(num_copies))
-    arr = np.tile(arr, num_copies)
-    print("\tDuplicated Input Shape: {}".format(arr.shape))
-    return arr
+            x = (x + slope_x) % width
+            y += slope_y
 
-def compute_trees(filename, slope_x, slope_y):
-    arr = preprocess_input(filename, slope_x, slope_y)
-    num_trees = 0
-    height, width = arr.shape[0], arr.shape[1]
-    position_x, position_y = 0, 0 
-    while position_x < width and position_y < height:
-        point = arr[position_y, position_x]
-        if point == "#":
-            num_trees += 1
+        solutions.append(num_trees)
+        print("Slope: {} ---> Num Trees: {}".format(slope, num_trees))
 
-        position_x += slope_x
-        position_y += slope_y
-
-    return num_trees
+    return solutions
 
 if __name__ == "__main__":
     infile = "input.txt"
-    slope_x, slope_y = 3, 1
-    num_trees = compute_trees(infile, slope_x, slope_y)
-    print("Number of trees hit: {}".format(num_trees))
+    slopes = [(3, 1)]
+    print("Solution: {}".format(solution(infile, slopes)))
--- 2020/3/p2.py
+++ 2020/3/p2.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python3
-import numpy as np
-from p1 import compute_trees
+from p1 import solution
+from functools import reduce
 
 if __name__ == "__main__":
     infile = "input.txt"
     slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-    num_trees = [compute_trees(infile, slope[0], slope[1]) for slope in slopes]
-    prod = np.prod(num_trees)
-    for slope, trees in zip(slopes, num_trees):
-        print("Slope: {}".format(slope))
-        print("\tTrees hit: {}".format(trees))
-    print("Product of trees hit: {}".format(prod))
+    trees = solution(infile, slopes)
+    prod = reduce(lambda x,y: x*y, trees)
+    print("Solution: {}".format(prod))
